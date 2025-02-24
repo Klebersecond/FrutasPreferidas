d063053813468ac957612976d00878482d791cfd
Kleber II <klebernssii@gmail.com>
2025-02-24 11:18:16 -0300

    Finaliza programa

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("##############");
+Console.WriteLine("### Frutas Favoritas ###");
+Console.WriteLine("###################");
+
+Console.WriteLine();
+
+Console.Write("1. ");
+Console.ForegroundColor =  ConsoleColor.DarkYellow;
+Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine("mexerica");
+
+Console.WriteLine("2. ");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("mamão");
+
+Console.WriteLine();
+
+Console.WriteLine("3. ");
+Console.ForegroundColor =  ConsoleColor.DarkRed;
+Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine("melancia");
+
+Console.WriteLine();
+
+Console.WriteLine("4. ");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Carambola");
